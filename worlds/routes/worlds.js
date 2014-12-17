39777cd84166effb547ea22d172f6ab4778453f5
Matthew Bohl <mbohl@ucsd.edu>
2014-12-17 13:43:13 -0800

    moved some libs to local/remote.  added index and new route worlds

--- worlds/routes/worlds.js
+++ worlds/routes/worlds.js
@@ -1,9 +1,20 @@
 var express = require('express');
+var worlds = require('../model/worlds');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.send('respond with a resource');
+
+    var world = new worlds(10, 10, 10, 30, 35, 35, 1, 3500, 5000);
+    res.send(JSON.stringify(world));
+});
+
+/* GET users listing. */
+router.get('/:iterations', function(req, res) {
+
+    var world = new worlds(10, 10, 10, 30, 35, 35, 1, 3500, 5000);
+    world.settle(req.params.iterations);
+    res.send(JSON.stringify(world));
 });
 
 module.exports = router;
